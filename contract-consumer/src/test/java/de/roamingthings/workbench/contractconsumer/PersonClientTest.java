5e45cc94243495daac4e2e3499e2fdefbd108506
Alexander Sparkowsky <info@roamingthings.de>
2019-06-16 09:05:32 +0200

    Update dependencies

--- contract-consumer/src/test/java/de/roamingthings/workbench/contractconsumer/PersonClientTest.java
+++ contract-consumer/src/test/java/de/roamingthings/workbench/contractconsumer/PersonClientTest.java
@@ -1,11 +1,9 @@
 package de.roamingthings.workbench.contractconsumer;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
@@ -13,22 +11,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.LOCAL;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = NONE)
-/*@AutoConfigureWireMock(port = 8080)
-@AutoConfigureJsonTesters*/
 @AutoConfigureStubRunner(
         ids = "de.roamingthings.workbench:contract-producer:+:8080",
         stubsMode = LOCAL
 )
-public class PersonClientTest {
-
+class PersonClientTest {
 
     @Autowired
     PersonClient personClient;
 
     @Test
-    public void should_retrieve_all_persons_from_producer() {
+    void should_retrieve_all_persons_from_producer() { // NOSONAR
         Person personOne = new Person("abc-123", "Paula Sorgenlos");
         Person personTwo = new Person("def-456", "Toni Tester");
 
